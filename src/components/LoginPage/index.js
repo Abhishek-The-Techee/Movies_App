259e79ecd893acad98e6f7369302a5311df540b9
Abhishek <abhishekktech2@gmail.com>
2023-09-19 10:01:17 +0000

    Login Route

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,21 +1,23 @@
 import {Component} from 'react'
+import Cookies from 'js-cookie'
+import {Redirect} from 'react-router-dom'
 
-import {
-  LoginBgContainer,
-  LogoContainer,
-  LogoImage,
-  LoginFormContainer,
-  FormContainer,
-  LoginHeading,
-  UserInput,
-  LabelText,
-  LoginButton,
-} from './styledComponents'
+import './index.css'
 
 class LoginForm extends Component {
   state = {
     userName: '',
     password: '',
+    showSubmitError: false,
+    errorMsg: '',
+  }
+
+  onChangeUserName = event => {
+    this.setState({userName: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
   }
 
   renderUserNameField = () => {
@@ -23,13 +25,19 @@ class LoginForm extends Component {
 
     return (
       <>
-        <LabelText htmlFor="user-name">USERNAME</LabelText>
-        <UserInput
+        <label className="label-text" htmlFor="user-name">
+          USERNAME
+        </label>
+        <br />
+        <input
+          className="user-input"
           type="text"
           id="user-name"
           value={userName}
           placeholder="Username"
+          onChange={this.onChangeUserName}
         />
+        <br />
       </>
     )
   }
@@ -39,35 +47,80 @@ class LoginForm extends Component {
 
     return (
       <>
-        <LabelText htmlFor="user-name">PASSWORD</LabelText>
-        <UserInput
+        <label className="label-text" htmlFor="user-name">
+          PASSWORD
+        </label>
+        <br />
+        <input
+          className="user-input"
           type="password"
           id="password"
           value={password}
           placeholder="Password"
+          onChange={this.onChangePassword}
         />
       </>
     )
   }
 
+  onSubmitSuccess = jwtToken => {
+    const {history} = this.props
+
+    Cookies.set('jwt_token', jwtToken, {expires: 30})
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => {
+    this.setState({showSubmitError: true, errorMsg})
+  }
+
+  onSubmitForm = async () => {
+    this.preventDefault()
+    const {userName, password} = this.state
+    const userDetails = {userName, password}
+    const loginApiUrl = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(loginApiUrl, options)
+    const data = response.json()
+    if (response.ok) {
+      this.onSubmitSuccess(data.jwt_token)
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
   render() {
+    const {showSubmitError, errorMsg} = this.state
+
+    const jwtToken = Cookies.get('jwt-token')
+    if (jwtToken !== undefined) {
+      return <Redirect to="/" />
+    }
+
     return (
-      <LoginBgContainer>
-        <LogoContainer>
-          <LogoImage
+      <div className="login-bg-container">
+        <div className="logo-container">
+          <img
             src="https://res.cloudinary.com/dnebrhaqh/image/upload/v1695027857/Group_7399_evjy6s.png"
             alt="logo"
+            className="logo-image"
           />
-        </LogoContainer>
-        <LoginFormContainer>
-          <FormContainer>
-            <LoginHeading>Login</LoginHeading>
+        </div>
+        <div className="login-form-container">
+          <form className="form-container" onSubmit={this.onSubmitForm}>
+            <h1 className="login-heading">Login</h1>
             {this.renderUserNameField()}
             {this.renderPasswordField()}
-            <LoginButton>Login</LoginButton>
-          </FormContainer>
-        </LoginFormContainer>
-      </LoginBgContainer>
+            {showSubmitError && <p className="error-msg">{errorMsg}</p>}
+            <button type="submit" className="login-btn">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
     )
   }
 }
